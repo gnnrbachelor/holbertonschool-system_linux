cdcd281b426595b0d30d08bb20b245359dd27966
typh0n1ankill <typh0n1ankill@typh0n1ankills-MacBook-Air.local>
2021-12-02 08:35:40 -0600

    Try out first step

---
+++ 0x00-ls/ls.h
@@ -0,0 +1,10 @@
+#ifndef LS_H
+#define LS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+
+#endif
+
+
---
+++ 0x00-ls/main.c
@@ -0,0 +1,34 @@
+#include "ls.h"
+
+/**
+ * main - entry
+ * @argc: num
+ * @argv: pointer to args
+ * Return: 0
+ *
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+	DIR *dir;
+	struct dirent *read;
+	char *dirname;
+
+	dirname = ".";
+	dir = opendir(dirname);
+	if (dir == NULL)
+	{
+		printf("Fuck off");
+	}
+
+	else
+	{
+		while ((read = readdir(dir)) != NULL)
+			printf("%s\n", read->d_name);
+	}
+	closedir(dir);
+
+}
+
