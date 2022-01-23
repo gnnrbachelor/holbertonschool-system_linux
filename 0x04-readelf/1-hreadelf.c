e7c9a6520106f21819de6073f87217a0d5f9a91d
root <gnnrbachelor@gmail.com>
2022-01-23 20:10:22 +0000

    Add remaining

---
+++ 0x04-readelf/1-hreadelf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: args
+ * @env:  environment
+ * Return: -1 or 0
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *args[] = {"/usr/bin/readelf", "-W", "-S", "", NULL};
+
+	(void)argc;
+	args[3] = argv[1];
+	if (execve("/usr/bin/readelf", args, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
