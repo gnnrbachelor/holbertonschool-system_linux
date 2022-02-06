b2d2c149e03304e495168c5d4640a09a4256507b
root <gnnrbachelor@gmail.com>
2022-02-06 09:19:57 +0000

    Fix strncmp

--- 0x05-libasm/2-strncmp.asm
+++ 0x05-libasm/2-strncmp.asm
@@ -7,46 +7,46 @@ BITS 64
 asm_strncmp:
         push    rbp
         mov     rbp, rsp
-        mov     QWORD  [rbp-24], rdi
-        mov     QWORD  [rbp-32], rsi
-        mov     QWORD  [rbp-40], rdx
-        mov     QWORD  [rbp-8], 0
-        mov     DWORD  [rbp-12], 0
+        mov     QWORD [rbp-24], rdi
+        mov     QWORD [rbp-32], rsi
+        mov     QWORD [rbp-40], rdx
+        mov     QWORD [rbp-8], 0
+        mov     DWORD [rbp-12], 0
         jmp     .L2
 .L4:
-        mov     rax, QWORD  [rbp-24]
-        movzx   eax, BYTE  [rax]
+        mov     rax, QWORD [rbp-24]
+        movzx   eax, BYTE [rax]
         movsx   edx, al
-        mov     rax, QWORD  [rbp-32]
-        movzx   eax, BYTE  [rax]
+        mov     rax, QWORD [rbp-32]
+        movzx   eax, BYTE [rax]
         movsx   ecx, al
         mov     eax, edx
         sub     eax, ecx
-        mov     DWORD  [rbp-12], eax
-        add     QWORD  [rbp-24], 1
-        add     QWORD  [rbp-32], 1
-        add     QWORD  [rbp-8], 1
+        mov     DWORD [rbp-12], eax
+        add     QWORD [rbp-24], 1
+        add     QWORD [rbp-32], 1
+        add     QWORD [rbp-8], 1
 .L2:
-        mov     rax, QWORD  [rbp-24]
-        movzx   eax, BYTE  [rax]
+        mov     rax, QWORD [rbp-24]
+        movzx   eax, BYTE [rax]
         test    al, al
         je      .L3
-        mov     rax, QWORD  [rbp-32]
-        movzx   eax, BYTE  [rax]
+        mov     rax, QWORD [rbp-32]
+        movzx   eax, BYTE [rax]
         test    al, al
         je      .L3
-        cmp     DWORD  [rbp-12], 0
+        cmp     DWORD [rbp-12], 0
         jne     .L3
-        mov     rax, QWORD  [rbp-8]
-        cmp     rax, QWORD  [rbp-40]
+        mov     rax, QWORD [rbp-8]
+        cmp     rax, QWORD [rbp-40]
         jb      .L4
 .L3:
-        cmp     DWORD  [rbp-12], 0
+        cmp     DWORD [rbp-12], 0
         jle     .L5
         mov     eax, 1
         jmp     .L6
 .L5:
-        cmp     DWORD  [rbp-12], 0
+        cmp     DWORD [rbp-12], 0
         jns     .L7
         mov     eax, -1
         jmp     .L6
