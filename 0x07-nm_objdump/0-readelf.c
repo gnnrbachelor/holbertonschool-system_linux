1b63f0d29bcbb5ec96b2be586bd3ed46e11add08
root <gnnrbachelor@gmail.com>
2022-03-19 17:50:01 +0000

    Test

---
+++ 0x07-nm_objdump/0-readelf.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry to main
+ * @argc: argc
+ * @argv: argv
+ * @env:  environ
+ * Return: -1 or 0
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *args[] = {"/usr/bin/readelf", "-W", "-h", "", NULL};
+
+	if (argc != 2)
+		exit(0);
+
+	args[3] = argv[1];
+	if (execve(args[0], args, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
