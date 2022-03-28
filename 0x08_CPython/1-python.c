0c65b7065d936859143aa2c00cfddd99b5ea2db3
root <gnnrbachelor@gmail.com>
2022-03-28 21:04:53 +0000

    Test first one

---
+++ 0x08_CPython/1-python.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list - Prints info about python list
+ * @p: PyObject pointer
+ * Return: Void
+ */
+
+
+void print_python_list(PyObject *p)
+{
+	int i;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < Py_SIZE(p); i++)
+		printf("Element %d: %s\n", i,
+		       Py_TYPE(PyList_GetItem(p, i))->tp_name);
+}
