91122d4bd8e7c8ac8519cea576226709d578f3db
root <gnnrbachelor@gmail.com>
2022-04-19 14:50:14 +0000

    Add task 1

---
+++ 0x09-strace/1-strace.c
@@ -0,0 +1,46 @@
+#include "syscalls.h"
+
+/**
+ * main - executes and traces a given command
+ * @argc:  arg count
+ * @argv: args
+ * @envp: Environment
+ * Return: 1 or 0
+ */
+
+int main(int argc, char *argv[], char *envp[])
+{
+	int index, status;
+	struct user_regs_struct user_regs;
+	pid_t pid;
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s <full_path> [path_args]\n", argv[0]);
+		return (1);
+	}
+
+	setbuf(stdout, NULL);
+	pid = fork();
+
+	if (pid == 0)
+	{
+		printf("execve");
+		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
+		execve(argv[1], argv + 1, envp);
+	}
+	else
+	{
+		for (status = 1, index = 1; !WIFEXITED(status); index ^= 1)
+		{
+			ptrace(PT_SYSCALL, pid, NULL, NULL);
+			wait(&status);
+			ptrace(PT_GETREGS, pid, NULL, &user_regs);
+			if (index)
+				printf("\n");
+			else
+				printf("%s", syscalls_64_g[user_regs.orig_rax].name);
+		}
+	}
+	return (0);
+}
