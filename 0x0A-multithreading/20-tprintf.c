d9c47cf7d540fdbcdc27b4c2ff9cbef515809d3e
root <gnnrbachelor@gmail.com>
2022-05-16 12:27:17 +0000

    Fix typo

--- 0x0A-multithreading/20-tprintf.c
+++ 0x0A-multithreading/20-tprintf.c
@@ -45,7 +45,7 @@ int tprintf(char const *format, ...)
 	num_char = printf("[%lu] ", pthread_self());
 	num_char += vprintf(format, args);
 	va_end(args);
-	if (pthread_mutex_unlock(&mutex) != 0);
+	if (pthread_mutex_unlock(&mutex) != 0)
 		perror(NULL);
 	return (num_char);
 
