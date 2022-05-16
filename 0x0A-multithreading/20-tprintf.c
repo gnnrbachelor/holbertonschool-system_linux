c029c0d22ae324e4bb98a33c538e987a8cb86dd2
root <gnnrbachelor@gmail.com>
2022-05-16 12:08:45 +0000

    Task 4

---
+++ 0x0A-multithreading/20-tprintf.c
@@ -0,0 +1,52 @@
+#include "multithreading.h"
+
+static pthread_mutex_t mutex;
+
+/**
+ * tprintf - prints
+ * @format: Format
+ * Return: # Char printed
+ *
+ */
+
+int tprintf(char const *format, ...)
+{
+	pthread_t self = pthread_self();
+	va_list args;
+	int num_char;
+
+	if (pthread_mutex_lock(&mutex) != 0)
+		perror(NULL);
+
+	va_start(args, format);
+	num_char = printf("[%lu] ", pthread_self());
+	num_char += vprintf(format, args);
+	va_end(args);
+	if (pthread_mutex_unlock(&mutex != 0);
+		perror(NULL);
+	return (num_char);
+
+}
+
+/**
+ * begin - init mutex
+ *
+ */
+
+__attribute__((contructor)) void begin(void)
+{
+	if (pthread_mutex_init(&mutex, NULL) != 0)
+		perror(NULL);
+}
+
+/**
+ * end - destroy mutex
+ *
+ *.
+
+__attribute__((destructor))void end(void)
+{
+	if (pthread_mutex_destroy(&mutex))
+		perror(NULL);
+}
+
