09264cc7a36452f365236431a3f2bb3ae6002ed9
root <gnnrbachelor@gmail.com>
2022-06-26 17:32:45 +0000

    Clean up for betty

--- 0x0C-sockets/3-server.c
+++ 0x0C-sockets/3-server.c
@@ -1,5 +1,11 @@
 #include "socket.h"
 
+void handle_error(char *err)
+{
+	perror(err);
+	exit(EXIT_FAILURE);
+}
+
 int main(void)
 {
 	int server, listen_stat, accept_fd, msg;
@@ -9,45 +15,26 @@ int main(void)
 
 	server = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (server == -1)
-	{
-		perror("Socket Error");
-		exit(EXIT_FAILURE);
-	}
-
+		handle_error("Socket Error");
 	sock.sin_family = AF_INET;
 	sock.sin_port = htons(PORT);
 	sock.sin_addr.s_addr = htonl(INADDR_ANY);
 	if (bind(server, (struct sockaddr *)&sock, sizeof(sock)) == -1)
-	{
-		perror("Bind Error");
-		exit(EXIT_FAILURE);
-	}
+		handle_error("Bind Error");
 	listen_stat = listen(server, 5);
 	if (listen_stat == -1)
-	{
-		perror("Listen Error");
-		exit(EXIT_FAILURE);
-	}
+		handle_error("Listen Error");
 	printf("Server listening to port %d\n", ntohs(sock.sin_port));
-	accept_fd = accept(server, (struct sockaddr *)&client, (socklen_t *) &client_size);
+	accept_fd = accept(server, (struct sockaddr *)&client,
+		(socklen_t *) &client_size);
 	if (accept_fd == -1)
-	{
-		perror("Accept Failed");
-		exit(EXIT_FAILURE);
-	}
+		handle_error("Accept Failed");
 	printf("Client connected: %s\n", inet_ntoa(client.sin_addr));
 	msg = recv(accept_fd, buf, 1024, 0);
 	if (msg == -1)
-	{
-		perror("Receive Failure");
-		exit(EXIT_FAILURE);
-	}
+		handle_error("Receive Failure");
 	close(accept_fd);
 	close(server);
 	printf("Message received: \"%s\"\n", buf);
 	return (0);
-
-
-
-
 }
